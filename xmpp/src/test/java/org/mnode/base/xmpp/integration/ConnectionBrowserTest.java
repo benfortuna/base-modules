5e68acc7fe9ecdb052d6d05b6e3be4ae6baee86d
fortuna <fortuna@6f6cb55d-b165-0410-8171-876e4bc4407c>
2009-04-13 07:41:47 +0000

    Create new connection to test browser service

--- xmpp/src/test/java/org/mnode/base/xmpp/integration/ConnectionBrowserTest.java
+++ xmpp/src/test/java/org/mnode/base/xmpp/integration/ConnectionBrowserTest.java
@@ -3,6 +3,8 @@
  */
 package org.mnode.base.xmpp.integration;
 
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
 import org.mnode.base.xmpp.ConnectionBrowser;
 import org.osgi.framework.ServiceReference;
 import org.springframework.osgi.test.AbstractConfigurableBundleCreatorTests;
@@ -13,8 +15,12 @@ import org.springframework.osgi.test.AbstractConfigurableBundleCreatorTests;
  */
 public class ConnectionBrowserTest extends AbstractConfigurableBundleCreatorTests {
 
-	public void testGetConnectionBrowser() {
+	public void testGetConnectionBrowser() throws XMPPException {
 		ServiceReference reference = bundleContext.getServiceReference(ConnectionBrowser.class.getName());
+//		ConnectionConfiguration config = new ConnectionConfiguration("jabber.org.au", 5222);
+		XMPPConnection connection = new XMPPConnection("jabber.org.au");
+		connection.connect();
+//		connection.login("benfortuna", "blw550");
 		ConnectionBrowser connectionBrowser = (ConnectionBrowser) bundleContext.getService(reference);
 		assertNotNull(connectionBrowser);
 	}
