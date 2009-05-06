b81a84acee92704cd8f51fb9f616065eb524f1e3
fortuna <fortuna@6f6cb55d-b165-0410-8171-876e4bc4407c>
2009-05-06 10:11:39 +0000

    Added license headers

--- account/src/main/java/org/mnode/base/account/internal/AccountManager.java
+++ account/src/main/java/org/mnode/base/account/internal/AccountManager.java
@@ -1,7 +1,26 @@
+/**
+ * This file is part of Base Modules.
+ *
+ * Copyright (c) 2009, Ben Fortuna [fortuna@micronode.com]
+ *
+ * Base Modules is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Base Modules is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Base Modules.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.mnode.base.account.internal;
 
 import java.util.List;
 
+import org.mnode.base.account.Account;
 import org.mnode.base.account.AccountListener;
 
 
@@ -17,13 +36,13 @@ public class AccountManager {
 		listeners.remove(listener);
 	}
 	
-	public void addAccount(Object account) {
+	public void addAccount(Account account) {
 		// persist account..
 		
 		fireAccountCreated(account);
 	}
 	
-	private void fireAccountCreated(Object account) {
+	private void fireAccountCreated(Account account) {
 		for (AccountListener l : listeners) {
 			l.accountCreated(account);
 		}
