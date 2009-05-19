eceb42349858e63f50d96eb696554b1e65bb9c16
fortuna <fortuna@6f6cb55d-b165-0410-8171-876e4bc4407c>
2009-05-19 12:13:07 +0000

    Refactored integration tests

---
+++ xmpp/src/integration/java/org/mnode/base/xmpp/integration/ConnectionListenerTest.java
@@ -0,0 +1,64 @@
+/**
+ * This file is part of Base Modules.
+ *
+ * Copyright (c) 2009, Ben Fortuna [fortuna@micronode.com]
+ *
+ * Base Modules is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Base Modules is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Base Modules.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.mnode.base.xmpp.integration;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jivesoftware.smack.ConnectionListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+
+public class ConnectionListenerTest extends AbstractXmppTest {
+
+	private static final Log LOG = LogFactory.getLog(ConnectionListenerTest.class);
+
+	public void testRegisterConnectionListener() throws XMPPException {
+		ConnectionListener listener = new ConnectionListener() {
+			@Override
+			public void connectionClosed() {
+				LOG.info("Connection closed");
+			}
+			@Override
+			public void connectionClosedOnError(Exception arg0) {
+				LOG.info("Connection closed - error: " + arg0);
+			}
+			@Override
+			public void reconnectingIn(int arg0) {
+				LOG.info("Reconnecting in: " + arg0);
+			}
+			@Override
+			public void reconnectionFailed(Exception arg0) {
+				LOG.info("Reconnection failed - error: " + arg0);
+			}
+			@Override
+			public void reconnectionSuccessful() {
+				LOG.info("Reconnection successful");
+			}
+		};
+		
+		bundleContext.registerService(ConnectionListener.class.getName(), listener, null);
+
+		XMPPConnection connection = new XMPPConnection("basepatterns.org");
+		connection.connect();
+		connection.login("test", "!password");
+		
+		connection.disconnect();
+	}
+
+}
