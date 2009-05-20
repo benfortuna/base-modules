eaad6d20f22e7eef2829086049168d11284018b2
fortuna <fortuna@6f6cb55d-b165-0410-8171-876e4bc4407c>
2009-05-20 10:13:02 +0000

    Refactored integration tests

---
+++ xmpp/src/it/java/org/mnode/base/xmpp/integration/ChatContextManagerImplTest.java
@@ -0,0 +1,56 @@
+/**
+ * This file is part of Base Modules.
+ *
+ * Copyright (c) 2009, Ben Fortuna [fortuna@micronode.com]
+ *
+ * Base Modules is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Base Modules is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Base Modules.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.mnode.base.xmpp.integration;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.ChatManagerListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.mnode.base.xmpp.ChatContext;
+import org.mnode.base.xmpp.ChatContextManager;
+import org.osgi.util.tracker.ServiceTracker;
+
+
+public class ChatContextManagerImplTest extends AbstractXmppTest {
+	
+	public void testGetChatContext() throws XMPPException {
+
+		ServiceTracker tracker = new ServiceTracker(bundleContext, ChatContextManager.class.getName(), null);
+		tracker.open();
+		final ChatContextManager contextManager = (ChatContextManager) tracker.getService();
+		assertNotNull(contextManager);
+		
+		final XMPPConnection connection = new XMPPConnection("basepatterns.org");
+		connection.connect();
+		connection.login("test", "!password");
+		
+		ChatManagerListener listener = new ChatManagerListener() {
+			@Override
+			public void chatCreated(Chat arg0, boolean arg1) {
+				ChatContext context = contextManager.getContext(arg0);
+				assertNotNull(context);
+				assertEquals(connection, context.getConnection());
+			}
+		};
+		bundleContext.registerService(ChatManagerListener.class.getName(), listener, null);
+		
+		connection.disconnect();
+	}
+
+}
---
+++ xmpp/src/it/java/org/mnode/base/xmpp/integration/RosterListenerTest.java
@@ -0,0 +1,69 @@
+/**
+ * This file is part of Base Modules.
+ *
+ * Copyright (c) 2009, Ben Fortuna [fortuna@micronode.com]
+ *
+ * Base Modules is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Base Modules is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Base Modules.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * 
+ */
+package org.mnode.base.xmpp.integration;
+
+import java.util.Collection;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jivesoftware.smack.RosterListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Presence;
+
+/**
+ * @author fortuna
+ *
+ */
+public class RosterListenerTest extends AbstractXmppTest {
+
+	private static final Log LOG = LogFactory.getLog(RosterListenerTest.class);
+	
+	public void testRegisterRosterListener() throws XMPPException {
+
+		RosterListener listener = new RosterListener() {
+			@Override
+			public void entriesAdded(Collection<String> arg0) {
+				LOG.info("Roster entries added: " + arg0);
+			}
+			@Override
+			public void entriesDeleted(Collection<String> arg0) {
+				LOG.info("Roster entries deleted: " + arg0);
+			}
+			@Override
+			public void entriesUpdated(Collection<String> arg0) {
+				LOG.info("Roster entries updated: " + arg0);
+			}
+			@Override
+			public void presenceChanged(Presence arg0) {
+				LOG.info("Presence changed: " + arg0.getFrom() + ", " + arg0.getStatus());
+			}
+		};
+		bundleContext.registerService(RosterListener.class.getName(), listener, null);
+
+		XMPPConnection connection = new XMPPConnection("basepatterns.org");
+		connection.connect();
+		connection.login("test", "!password");
+		connection.disconnect();
+	}
+
+}
