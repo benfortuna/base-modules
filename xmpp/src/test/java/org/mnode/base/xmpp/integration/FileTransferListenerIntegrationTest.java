c6b21f062f9408ecc09ca2e93eb02b5fc61a6309
fortuna <fortuna@6f6cb55d-b165-0410-8171-876e4bc4407c>
2009-06-14 07:00:28 +0000

    Refactored integration tests

---
+++ xmpp/src/test/java/org/mnode/base/xmpp/integration/FileTransferListenerIntegrationTest.java
@@ -0,0 +1,52 @@
+/**
+ * This file is part of Base Modules.
+ *
+ * Copyright (c) 2009, Ben Fortuna [fortuna@micronode.com]
+ *
+ * Base Modules is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Base Modules is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Base Modules.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.mnode.base.xmpp.integration;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smackx.filetransfer.FileTransferListener;
+import org.jivesoftware.smackx.filetransfer.FileTransferRequest;
+
+public class FileTransferListenerIntegrationTest extends AbstractXmppIntegrationTest {
+
+    private static final Log LOG = LogFactory
+            .getLog(FileTransferListenerIntegrationTest.class);
+
+    public void testRegisterFileTransferListener() throws XMPPException {
+        FileTransferListener listener = new FileTransferListener() {
+            @Override
+            public void fileTransferRequest(FileTransferRequest request) {
+                LOG.info("File transfer requested: " + request.getFileName()
+                        + " (" + request.getFileSize() + " bytes)");
+            }
+        };
+
+        bundleContext.registerService(FileTransferListener.class.getName(),
+                listener, null);
+
+        XMPPConnection connection = new XMPPConnection("basepatterns.org");
+        connection.connect();
+        connection.login("test", "!password");
+
+        connection.disconnect();
+    }
+
+}
