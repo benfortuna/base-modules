26550f311d49838edba0de5150c74d5e72e8cfb1
fortuna <fortuna@6f6cb55d-b165-0410-8171-876e4bc4407c>
2009-06-16 13:03:29 +0000

    Updated license header

--- mail/src/main/java/org/mnode/base/mail/MailboxMessage.java
+++ mail/src/main/java/org/mnode/base/mail/MailboxMessage.java
@@ -1,25 +1,21 @@
-/*
- * $Id: MailboxMessage.java $
+/**
+ * This file is part of Base Modules.
  *
- * Created: [13/09/2008]
+ * Copyright (c) 2009, Ben Fortuna [fortuna@micronode.com]
  *
- * Copyright (c) 2008, Ben Fortuna
+ * Base Modules is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
+ * Base Modules is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * You should have received a copy of the GNU General Public License
+ * along with Base Modules.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.mnode.base.mail;
 
 import javax.mail.Address;
