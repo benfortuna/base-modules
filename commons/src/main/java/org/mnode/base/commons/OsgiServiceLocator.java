3a0b1bc40a74e636b9fc03df04f7c5894e77403b
fortuna <fortuna@6f6cb55d-b165-0410-8171-876e4bc4407c>
2009-06-17 10:21:19 +0000

    Added service locator support

---
+++ commons/src/main/java/org/mnode/base/commons/OsgiServiceLocator.java
@@ -0,0 +1,65 @@
+/**
+ * This file is part of Base Modules.
+ *
+ * Copyright (c) 2009, Ben Fortuna [fortuna@micronode.com]
+ *
+ * Base Modules is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Base Modules is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Base Modules.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.mnode.base.commons;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.util.tracker.ServiceTracker;
+
+/**
+ * @author fortuna
+ *
+ */
+public class OsgiServiceLocator implements ServiceLocator {
+
+    private final BundleContext context;
+    
+    private final Map<Class<?>, ServiceTracker> serviceTrackers;
+    
+    /**
+     * @param context the bundle context in which to find services
+     */
+    public OsgiServiceLocator(BundleContext context) {
+        this.context = context;
+        serviceTrackers = new HashMap<Class<?>, ServiceTracker>();
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @SuppressWarnings("unchecked")
+    public <T> T findService(Class<T> type) {
+        ServiceTracker tracker = serviceTrackers.get(type);
+        if (tracker == null) {
+            synchronized (serviceTrackers) {
+                tracker = serviceTrackers.get(type);
+                if (tracker == null) {
+                    tracker = new ServiceTracker(context, type.getName(), null);
+                    tracker.open();
+                    serviceTrackers.put(type, tracker);
+                }
+            }
+        }
+        return (T) tracker.getService();
+    }
+
+}
---
+++ commons/src/test/java/org/mnode/base/commons/OsgiServiceLocatorTest.java
@@ -0,0 +1,102 @@
+/**
+ * This file is part of Base Modules.
+ *
+ * Copyright (c) 2009, Ben Fortuna [fortuna@micronode.com]
+ *
+ * Base Modules is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Base Modules is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Base Modules.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.mnode.base.commons;
+
+import junit.framework.Assert;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceListener;
+import org.osgi.framework.ServiceReference;
+
+
+/**
+ * @author fortuna
+ *
+ */
+public class OsgiServiceLocatorTest {
+
+    private OsgiServiceLocator serviceLocator;
+    
+    private BundleContext bundleContext;
+    
+    private ServiceReference serviceReference;
+    
+    private String service;
+    
+    private Mockery mockContext;
+    
+    @Before
+    public void runBeforeEachTest() {
+        mockContext = new Mockery();
+        
+        bundleContext = mockContext.mock(BundleContext.class);
+        
+        serviceReference = mockContext.mock(ServiceReference.class);
+        
+        service = "TestService";
+        
+        serviceLocator = new OsgiServiceLocator(bundleContext);
+    }
+    
+    @After
+    public void assertExpections() {
+        mockContext.assertIsSatisfied();
+    }
+    
+    @Test
+    public void testFindService() throws InvalidSyntaxException {
+        mockContext.checking(new Expectations() {
+            {
+                one(bundleContext).createFilter(with(any(String.class)));
+                one(bundleContext).addServiceListener(with(any(ServiceListener.class)),
+                        with(any(String.class)));
+                one(bundleContext).getServiceReferences(with(any(String.class)), with(any(String.class)));
+                    will(returnValue(new ServiceReference[] { serviceReference }));
+                one(bundleContext).getService(serviceReference);
+                    will(returnValue(service));
+            }
+        });
+        
+        String foundService = serviceLocator.findService(String.class);
+        Assert.assertEquals(service, foundService);
+    }
+    
+    @Test
+    public void testFindServiceNotAvailable() throws InvalidSyntaxException {
+        mockContext.checking(new Expectations() {
+            {
+                one(bundleContext).createFilter(with(any(String.class)));
+                one(bundleContext).addServiceListener(with(any(ServiceListener.class)),
+                        with(any(String.class)));
+                one(bundleContext).getServiceReferences(with(any(String.class)), with(any(String.class)));
+                    will(returnValue(new ServiceReference[] { }));
+            }
+        });
+        
+        Integer foundService = serviceLocator.findService(Integer.class);
+        Assert.assertNull(foundService);
+    }
+}
