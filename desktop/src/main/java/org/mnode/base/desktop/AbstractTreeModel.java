97b1c73ed5794a1311651c68ff6361fe0955f054
fortuna <fortuna@6f6cb55d-b165-0410-8171-876e4bc4407c>
2009-06-18 12:05:45 +0000

    Added desktop module

---
+++ desktop/src/main/java/org/mnode/base/desktop/AbstractTreeModel.java
@@ -0,0 +1,78 @@
+/**
+ * This file is part of Base Modules.
+ *
+ * Copyright (c) 2009, Ben Fortuna [fortuna@micronode.com]
+ *
+ * Base Modules is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Base Modules is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Base Modules.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ *
+ */
+package org.mnode.base.desktop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.event.TreeModelEvent;
+import javax.swing.event.TreeModelListener;
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreePath;
+
+/**
+ * An abstract tree model implementation.
+ * @author Ben Fortuna
+ */
+public abstract class AbstractTreeModel implements TreeModel {
+
+	private List<TreeModelListener> listeners;
+
+	/**
+	 * Default constructor.
+	 */
+	public AbstractTreeModel() {
+		listeners = new ArrayList<TreeModelListener>();
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.swing.tree.TreeModel#valueForPathChanged(javax.swing.tree.TreePath, java.lang.Object)
+	 */
+	public void valueForPathChanged(TreePath path, Object newValue) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.swing.tree.TreeModel#addTreeModelListener(javax.swing.event.TreeModelListener)
+	 */
+	public void addTreeModelListener(TreeModelListener listener) {
+		listeners.add(listener);
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.swing.tree.TreeModel#removeTreeModelListener(javax.swing.event.TreeModelListener)
+	 */
+	public void removeTreeModelListener(TreeModelListener listener) {
+		listeners.remove(listener);
+	}
+	
+	/**
+	 * @param path
+	 */
+	protected void fireTreeStructureChanged(TreePath path) {
+		TreeModelEvent e = new TreeModelEvent(this, path);
+		for (TreeModelListener l : listeners) {
+			l.treeStructureChanged(e);
+		}
+	}
+}
---
+++ desktop/src/main/java/org/mnode/base/desktop/EnhancedComboBoxRenderer.java
@@ -0,0 +1,46 @@
+/**
+ * This file is part of Base Modules.
+ *
+ * Copyright (c) 2009, Ben Fortuna [fortuna@micronode.com]
+ *
+ * Base Modules is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Base Modules is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Base Modules.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.mnode.base.desktop;
+
+import java.awt.Component;
+
+import javax.swing.JList;
+import javax.swing.JSeparator;
+import javax.swing.plaf.basic.BasicComboBoxRenderer;
+
+/**
+ * Enhance the default combo box renderer by adding support for separators.
+ * @author Ben Fortuna
+ */
+public class EnhancedComboBoxRenderer extends BasicComboBoxRenderer {
+	
+	private static final long serialVersionUID = 1768975539253158314L;
+
+	/* (non-Javadoc)
+	 * @see javax.swing.plaf.basic.BasicComboBoxRenderer#getListCellRendererComponent(javax.swing.JList, java.lang.Object, int, boolean, boolean)
+	 */
+	public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+		Component defaultRenderer = super.getListCellRendererComponent(list, value, index, isSelected,
+				cellHasFocus);
+		if (value instanceof JSeparator) {
+			return (Component) value;
+		}
+		return defaultRenderer;
+	}
+}
