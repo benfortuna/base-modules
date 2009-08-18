64ab55738ed5977494945add44c935d95fb468d9
fortuna <fortuna@6f6cb55d-b165-0410-8171-876e4bc4407c>
2009-08-18 13:44:02 +0000

    Added proxy support for OSGi services

---
+++ commons/src/main/java/org/mnode/base/commons/ServiceProxy.java
@@ -0,0 +1,68 @@
+/**
+ * This file is part of Base Modules.
+ *
+ * Copyright (c) 2009, Ben Fortuna [fortuna@micronode.com]
+ *
+ * Base Modules is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Base Modules is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Base Modules.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.mnode.base.commons;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.util.tracker.ServiceTracker;
+
+/**
+ * @author fortuna
+ *
+ */
+public class ServiceProxy {
+
+    /**
+     * @param <T> the service type
+     * @param classLoader the classloader for the proxy
+     * @param serviceClass the service class to proxy
+     * @param context the bundle context use to locate the underlying service
+     * @return a new proxy for the specified service type
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T newProxyInstance(ClassLoader classLoader, Class<T> serviceClass, BundleContext context) {
+        return (T) Proxy.newProxyInstance(classLoader, new Class<?>[] { serviceClass},
+                new ServiceInvocationHandler(serviceClass, context));
+    }
+    
+    private static class ServiceInvocationHandler implements InvocationHandler {
+        
+        private final ServiceTracker tracker;
+        
+        public ServiceInvocationHandler(Class<?> serviceClass, BundleContext context) {
+            tracker = new ServiceTracker(context, serviceClass.getName(), null);
+            tracker.open();
+        }
+        
+        /**
+         * {@inheritDoc}
+         */
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            Object service = tracker.getService();
+            if (service == null) {
+                throw new ServiceNotAvailableException();
+            }
+            return method.invoke(service, args);
+        }
+    }
+}
