2b1f8e74ab56889dbed6b1370d7157e267db5b14
fortuna <fortuna@6f6cb55d-b165-0410-8171-876e4bc4407c>
2009-09-06 09:17:59 +0000

    Added structured log message

---
+++ log/src/main/java/org/mnode/base/log/LogMessage.java
@@ -0,0 +1,35 @@
+/**
+ * This file is part of Base Modules.
+ *
+ * Copyright (c) 2009, Ben Fortuna [fortuna@micronode.com]
+ *
+ * Base Modules is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Base Modules is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Base Modules.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.mnode.base.log;
+
+/**
+ * Defines a structured log message.
+ * 
+ * @author fortuna
+ *
+ */
+public interface LogMessage {
+
+    /**
+     * @param args optional arguments to include in the message
+     * @return a formatted log message
+     */
+    String format(Object...args);
+}
