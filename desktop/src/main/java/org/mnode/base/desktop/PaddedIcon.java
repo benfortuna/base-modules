261b1d75b1af15851028b60946849cc6be18c8ca
fortuna <fortuna@6f6cb55d-b165-0410-8171-876e4bc4407c>
2010-03-09 12:51:14 +0000

    Added icon padding implementation

---
+++ desktop/src/main/java/org/mnode/base/desktop/PaddedIcon.java
@@ -0,0 +1,70 @@
+/**
+ * This file is part of Base Modules.
+ *
+ * Copyright (c) 2010, Ben Fortuna [fortuna@micronode.com]
+ *
+ * Base Modules is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Base Modules is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Base Modules.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.mnode.base.desktop;
+
+import java.awt.Component;
+import java.awt.Graphics;
+
+import javax.swing.Icon;
+
+/**
+ * @author Ben
+ *
+ */
+public class PaddedIcon implements Icon {
+
+    private final Icon icon;
+    
+    private final int width;
+    
+    private final int height;
+    
+    public PaddedIcon(Icon icon, int width, int height) {
+        this.icon = icon;
+        this.width = Math.max(width, icon.getIconWidth());
+        this.height = Math.max(height, icon.getIconHeight());
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getIconHeight() {
+        return height;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getIconWidth() {
+        return width;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void paintIcon(Component c, Graphics g, int x, int y) {
+        icon.paintIcon(c, g, (int) (x + (width - icon.getIconWidth()) / 2),
+                (int) (y + (height - icon.getIconHeight()) / 2));
+    }
+
+}
